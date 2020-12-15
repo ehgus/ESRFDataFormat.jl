8545977476882fbb62c6acb9f8412a573b741140
Dohyeon Lee <dleh428@kaist.ac.kr>
2020-12-15 23:10:36 +0900

    data header method

--- src/ESRFData.jl
+++ src/ESRFData.jl
@@ -1,6 +1,10 @@
 
 
-struct ESRFData
+mutable struct ESRFData 
     header::AbstractDict
     data::AbstractArray
 end
+
+@inline header(d::ESRFData) = d.header
+
+@inline data(d::ESRFData) = d.data
